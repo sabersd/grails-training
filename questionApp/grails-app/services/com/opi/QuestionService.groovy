6b312cd7a2bf96a4e8b3773e352a63cd926361db
sabersd <sabersd@gmail.com>
2014-07-23 01:53:41 -0700

    forgot the service

---
+++ questionApp/grails-app/services/com/opi/QuestionService.groovy
@@ -0,0 +1,31 @@
+package com.opi
+
+import grails.transaction.Transactional
+import grails.plugin.cache.CacheEvict
+import grails.plugin.cache.Cacheable
+import grails.plugin.cache.CachePut
+
+
+@Transactional
+class QuestionService {
+	@Cacheable('question')
+	def getQuestion(String questionId) {
+		println "fetching question"
+		def question = Question.get(questionId)
+		return question
+	}
+
+	@CachePut(value = 'question', key = '#question.id')
+	void save(Question question) {
+		println "Saving question $question"
+		message.save()
+	}
+
+	@CacheEvict(value = 'question', key = '#question.id')
+	void delete(Question question) {
+		println "Deleting question $question"
+		message.delete()
+	}
+}
+
+
